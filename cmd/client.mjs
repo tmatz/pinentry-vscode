bd518805df3b972b66e9433f417393f828a4a39e
MATSUDA Takashi <tmatz.dev@gmail.com>
2023-11-04 02:22:42 +0900

    client.mjs

--- cmd/client.mjs
+++ cmd/client.mjs
@@ -1,10 +1,5 @@
 import net from "net";
-
 const socketFile = "/run/user/1000/pinentry-vscode.sock";
-
 const socket = net.createConnection(socketFile);
-socket.on("data", (data) => {
-  console.log("received:", data.toString());
-  socket.end();
-});
-socket.write("GETPIN\n");
+process.stdin.pipe(socket);
+socket.pipe(process.stdout);
